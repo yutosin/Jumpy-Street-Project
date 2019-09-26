8c3256a2494ae2d7232c1486f5ee7b064bbd2f7f
nas <nmuhammad121@gmail.com>
2019-09-25 23:08:44 -0400

    Cell/Grid Movement Beginnings -Started to outline the class structure and play around with design concepts for "grid" system -Added new scene for testing level generation (terrain strip management) system -Added two new classes (TerrainStrip and TerrainStripFactory) that will form basis of the level generation system -Added some materials that will be used for terrain strips

---
+++ Assets/Scripts/TerrainStrip.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TerrainType
+{
+    Grass,
+    River,
+    Road,
+    Train
+}
+
+public struct Cell
+{
+    public bool accessible;
+    public Vector3 gridPos;
+}
+
+public class TerrainStrip : MonoBehaviour
+{
+    private List<Cell> _cells;
+
+    public Material TerrainMat;
+    public TerrainType Type;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cells = new List<Cell>(20);
+    }
+
+    public void CreateCells(float zPos, Prop prop = new Prop())
+    {
+        float xBase = -9.5f;
+        float xInc = 0.5f;
+        for (int i = 0; i < _cells.Count; i++)
+        {
+            int objectChance = Random.Range(1, 4);
+            
+            Cell tempCell = new Cell();
+            tempCell.gridPos = new Vector3(xBase + (i * xInc), 0, zPos);
+            
+            if (objectChance == 1 && prop.propPrefab != null)
+            {
+                Instantiate(prop.propPrefab, tempCell.gridPos, Quaternion.identity);
+                tempCell.accessible = prop.propAccessibility;
+            }
+            
+            _cells[i] = tempCell;
+        }
+    }
+}
---
+++ Assets/Scripts/TerrainStripFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Prop
+{
+    public GameObject propPrefab;
+    public bool propAccessibility;
+    public TerrainType propTerrain;
+}
+
+public class TerrainStripFactory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
