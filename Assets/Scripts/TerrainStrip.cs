1a95e8bb591cdf17873d60317c638e3dfd1c39eb
nas <nmuhammad121@gmail.com>
2019-09-29 16:27:56 -0400

    Basic Terrain Strip Generation -TerrainStripFactory can now be attached to an object and on start will generate twenty random terrain strips -Added the TerrainInfo struct which contains info on a Terrain Material and its actual type; helpful for encapsulation -Made prop struct serializable so that it is possible to drag and drop prefabs into editor -Removed TerrainMat public variable from TerrainStrip for encapsulation; type will soon be made private as well with getter -added SetupTerrainStrip which is called by TerrainStripFactory and calls private setup member functions -Create a terrain strip prefab with terrain strip script attached -Created some test props for the terrain strips

--- Assets/Scripts/TerrainStrip.cs
+++ Assets/Scripts/TerrainStrip.cs
@@ -16,24 +16,43 @@ public struct Cell
     public Vector3 gridPos;
 }
 
+[System.Serializable]
+public struct TerrainInfo
+{
+    public TerrainType type;
+    public Material mat;
+}
+
 public class TerrainStrip : MonoBehaviour
 {
     private List<Cell> _cells;
-
-    public Material TerrainMat;
-    public TerrainType Type;
+    private TerrainInfo _terrainInfo;
     
-    // Start is called before the first frame update
-    void Start()
+    public TerrainType Type;
+
+    public void SetupTerrainStrip(TerrainInfo terrainInfo, Prop prop)
     {
         _cells = new List<Cell>(20);
+        SetTerrainInfo(terrainInfo);
+        CreateCells(transform.position.z, prop);
+    }
+
+    private void SetTerrainInfo(TerrainInfo terrainInfo)
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        if (rend != null)
+        {
+            _terrainInfo = terrainInfo;
+            rend.material = _terrainInfo.mat;
+            Type = _terrainInfo.type;
+        }
     }
 
     public void CreateCells(float zPos, Prop prop = new Prop())
     {
         float xBase = -9.5f;
-        float xInc = 0.5f;
-        for (int i = 0; i < _cells.Count; i++)
+        float xInc = 1;
+        for (int i = 0; i < _cells.Capacity; i++)
         {
             int objectChance = Random.Range(1, 4);
             
@@ -42,11 +61,11 @@ public class TerrainStrip : MonoBehaviour
             
             if (objectChance == 1 && prop.propPrefab != null)
             {
-                Instantiate(prop.propPrefab, tempCell.gridPos, Quaternion.identity);
+                Instantiate(prop.propPrefab, new Vector3(tempCell.gridPos.x, 1, tempCell.gridPos.z), Quaternion.identity);
                 tempCell.accessible = prop.propAccessibility;
             }
             
-            _cells[i] = tempCell;
+            _cells.Add(tempCell);
         }
     }
 }
