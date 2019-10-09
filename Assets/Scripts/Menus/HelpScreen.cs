a455995f3e87ea112ba366b5dfb3ddf377b768b8
ZayZenBlaze <isaias9879@gmail.com>
2019-10-09 16:19:58 -0400

    Menu Additions/Scripts
    Created Menus and the Scripts along with them.

---
+++ Assets/Scripts/Menus/HelpScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HelpScreen : MonoBehaviour
+{
+    public Button Returnbutton;
+    public Button QuitGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnReturnButtonClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
