8e48dcd0f9a3397fc4691b29bead47a9f00b248c
ZayZenBlaze <isaias9879@gmail.com>
2019-10-16 16:04:29 -0400

    Simple Collision Additions
    Added a "Collsion" Folder and script to the project. Also typed in a small amount of code and added comments regarding what it should do.

---
+++ Assets/Scripts/Collisions/VehicleCollisions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VehicleCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+        //Handles the actions involving the player and the different in-game vehicles
+    {
+        if(collision.gameObject.tag == "Vehicle")
+            //holds the code that tells the game to reset the scene once the player hits or gets hit by a vehicle
+        {
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
